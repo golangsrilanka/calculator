bd9c1b15ce2027e63935f3834d243aa96b6beb1d
Charuka Samarakoon <charuka@protonmail.com>
2022-01-21 19:43:48 +0530

    Add division

---
+++ cmd/div.go
@@ -0,0 +1,56 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+// divCmd represents the div command
+var divCmd = &cobra.Command{
+	Use:   "div",
+	Short: "Divide two numbers",
+	Long: `Divide two numbers passed as args
+	
+	Example:
+	go run main.go div 3 2
+	-> 1.5`,
+	Run: func(cmd *cobra.Command, args []string) {
+		num1, _ := strconv.ParseFloat(args[0], 64)
+		num2, _ := strconv.ParseFloat(args[1], 64)
+
+		total := num1 / num2
+
+		fmt.Println(total)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(divCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// divCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// divCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
