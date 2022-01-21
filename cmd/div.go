7d1f77934d8a1972273f46280bccd3bc12ac9f35
HasinduLanka <hlrathnayaka@gmail.com>
2022-01-21 19:43:47 +0530

    Command Documentation

--- cmd/div.go
+++ cmd/div.go
@@ -25,17 +25,12 @@ import (
 // mulCmd represents the mul command
 var divCmd = &cobra.Command{
 	Use:   "div",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Divide two numbers",
+	Long:  `Divide one number by another`,
 	Run: func(cmd *cobra.Command, args []string) {
 		num1, _ := strconv.ParseUint(args[0], 10, 32)
 		num2, _ := strconv.ParseUint(args[1], 10, 32)
-		tot := num1 / num2
+		tot := float64(num1) / float64(num2)
 		fmt.Println(tot)
 	},
 }
