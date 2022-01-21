a00344906809fd4aceb039452cc631daed581108
kesaralive@gmail.com <90370604+kesaralive@users.noreply.github.com>
2022-01-21 19:55:18 +0530

    feat: create substraction operation

---
+++ cmd/sub.go
@@ -0,0 +1,55 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+// mulCmd represents the mul command
+var subCmd = &cobra.Command{
+	Use:   "sub",
+	Short: "Use to substract two numbers. add [NUM1] [NUM2]",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		num1, _ := strconv.ParseUint(args[0], 10, 32)
+		num2, _ := strconv.ParseUint(args[1], 10, 32)
+		tot := num1 - num2
+		fmt.Println(tot)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(subCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// mulCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// mulCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
