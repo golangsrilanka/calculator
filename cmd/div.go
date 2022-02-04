49348455788d5a23f29851c6fe6146010b74e67a
jellybee <buddhini.abay@gmail.com>
2022-02-04 09:43:22 +0530

    feat: add sub and div functionalities

--- cmd/div.go
+++ cmd/div.go
@@ -1,17 +1,6 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 */
 package cmd
 
@@ -22,33 +11,42 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// addCmd represents the add command
-var addCmd = &cobra.Command{
-	Use:   "add",
+// divCmd represents the div command
+var divCmd = &cobra.Command{
+	Use:   "div",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
+
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		num1, _ := strconv.ParseUint(args[0], 10, 32)
 		num2, _ := strconv.ParseUint(args[1], 10, 32)
-		tot := num1 + num2
-		fmt.Println(tot)
+
+		//for when the denominator is zero answer is undefined
+		if num2 == 0 {
+			fmt.Println("Answer is Undefined")
+
+		} else {
+			ans := float64(num1) / float64(num2)
+			fmt.Println(ans)
+		}
+
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(addCmd)
+	rootCmd.AddCommand(divCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// divCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// divCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
